88a805491a217346f318907a3dd0efabb7574264
Atur Aritonang <mangatur.aritonang@gmail.com>
2024-12-09 10:12:13 +0700

    Seeding

--- ECommerce/ECommerce.WebApp/Models/Data/ECommerceDbContext.cs
+++ ECommerce/ECommerce.WebApp/Models/Data/ECommerceDbContext.cs
@@ -2,7 +2,7 @@
 
 namespace ECommerce.WebApp.Models.Data;
 
-public class ECommerceDbContext: DbContext
+public class ECommerceDbContext : DbContext
 {
     public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
     {
@@ -10,6 +10,19 @@ public class ECommerceDbContext: DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Seed();
         modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+    }
+}
+
+public static class ModelBuilderExtensions
+{
+    public static void Seed(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .HasData(
+                new Category() { Id = 1, ShortName = "Food", LongName = "Food", Description = "Fresh food for you", Active = true },
+                new Category() { Id = 2, ShortName = "Drink", LongName = "Drink", Description = "Best serve", Active = true },
+                new Category() { Id = 3, ShortName = "Dessert", LongName = "Dessert", Description = "Dessert will make your day", Active = true });
     }
 }
